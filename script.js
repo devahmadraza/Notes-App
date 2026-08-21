0d089b86b6acb8456af5683c1dd5f9385c371433
devahmadraza <devahmadraza296@gmail.com>
2026-08-21 11:35:54 +0500

    Complete Notes App

--- script.js
+++ script.js
@@ -1,70 +1,97 @@
-const titleInput=document.querySelector('.title-input')
-const descriptionInput=document.querySelector('.description-input')
-const addBtn=document.querySelector('.add-btn')
-const error=document.querySelector('.error')
-const searchInput=document.querySelector('.search-input')
-const noteCount=document.querySelector('.note-count')
-const notesContainer=document.querySelector('.notes-container')
+
+const titleInput = document.querySelector('.title-input')
+const descriptionInput = document.querySelector('.description-input')
+const addBtn = document.querySelector('.add-btn')
+const error = document.querySelector('.error')
+const searchInput = document.querySelector('.search-input')
+const noteCount = document.querySelector('.note-count')
+const notesContainer = document.querySelector('.notes-container')
 
 
-const notes=[]
+const notes = []
+let editIndex = null
 
+addBtn.addEventListener('click', () => {
+    const title = titleInput.value
+    const description = descriptionInput.value
 
-addBtn.addEventListener('click',()=>{
-    const title=titleInput.value
-    const description=descriptionInput.value
-
-    if (title===''  || description==='') {
-        error.textContent='Please fill all the fields';
-       return 
+    if (title === '' || description === '') {
+        error.textContent = 'Please fill all the fields';
+        return
     }
 
-    const note={
-    title:title,
-    description:description,
-}
+    if (editIndex !== null) {
+        notes[editIndex].title = title
+        notes[editIndex].description = description
+
+        editIndex = null
+
+        addBtn.textContent = 'Add Note'
+    } else {
+        const note = {
+            title: title,
+            description: description,
+        }
+
+        notes.push(note);
+    }
+
+    titleInput.value = ''
+    descriptionInput.value = ''
 
 
-    notes.push(note);
-    titleInput.value=''
-    
-    descriptionInput.value=''
-    console.log(notes)
-    
-    error.textContent=''
+    error.textContent = ''
     renderNotes()
-})  
+})
+
 
 
 
 function renderNotes() {
-    notesContainer.innerHTML=''
-    if (notes.length == 0) {    
-        notesContainer.innerHTML= `<p class='empty-message'>No Notes Available</p>` 
-      }
-notes.forEach(function (note) {
-    const card = document.createElement('div')
-card.classList.add('note-card')
-card.innerHTML=`
-<h3>${note.title}</h3>
-<p>${note.description}</p>
-<button  class="edit-btn">Edit</button>
-<button  class="del-btn">Delete</button>
-`
-notesContainer.appendChild(card)
+    notesContainer.innerHTML = ''
+    if (notes.length == 0) {
+        notesContainer.innerHTML = `<p class='empty-message'>No Notes Available</p>`
+    }else{
+    notes.forEach(function (note, index) {
+        const card = document.createElement('div')
+        card.classList.add('note-card')
+        card.innerHTML = `
+        <h3>${note.title}</h3>
+        <p>${note.description}</p>
+        <button  class="edit-btn">Edit</button>
+        <button  class="del-btn">Delete</button>
+        `
+        notesContainer.appendChild(card)
+        // DELETE BTN
 
-})
-noteCount.textContent=notes.length
+        const delBtn = card.querySelector('.del-btn')
+        delBtn.addEventListener('click', () => {
+            notes.splice(index, 1)
+            renderNotes()
+        })
+
+        // END DELETE BTN
+
+        //  EDIT BTN
+
+        const editBtn = card.querySelector('.edit-btn')
+        editBtn.addEventListener('click', () => {
+
+            titleInput.value = note.title;
+            descriptionInput.value = note.description;
+
+            editIndex = index
+            
+            addBtn.textContent = 'Update Note'
+            titleInput.focus()
+        })
+
+        // END EDIT BTN
+    })
+    }
+    noteCount.textContent = notes.length
 }
 
-const delBtn=document.querySelector('.del-btn')
-function deleteNote() {
-delBtn.addEventListener('click',()=>{
 
 
-})
-    
-}
-
-
-        renderNotes()
+renderNotes()
